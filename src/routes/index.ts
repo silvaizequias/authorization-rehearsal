0177f96b3f50926347281038d51cfbcb1ef92312
silvaizequias <izequias.silva@atmansystems.com>
2022-09-16 14:54:30 -0300

    refactoring

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,25 +3,12 @@ import httpProxy from 'express-http-proxy'
 import { MicroServices } from '../microservices'
 import { Authorizations } from '../middlewares/authorizatios.middleware'
 
-const createError = require('http-errors')
-
 const router = express.Router()
 
 const server1 = httpProxy(MicroServices.ms1)
 const server2 = httpProxy(MicroServices.ms2)
 
 const authProfile = Authorizations
-
-router.use( async (req, res, next) => {
-    next(createError.NotFound('Route not Found'))
-})
-
-router.use( (err, req, res, next) => {
-    res.status(err.status || 500).json({
-        status: false,
-        message: err.message
-    })
-})
 
 router.get('/', (_, res) => {
   res.status(200).send('Attlas Authorization Rehearsal!')
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,22 @@ import express from 'express'
 const app = express()
 const port = process.env.SERVER_PORT || 3300
 
+const createError = require('http-errors')
+
 app.use(express.json())
 
 app.use('/', route)
+
+app.use( async (req, res, next) => {
+  next(createError.NotFound('Route not Found'))
+})
+
+app.use( (err, req, res, next) => {
+  res.status(err.status || 500).json({
+      status: false,
+      message: err.message
+  })
+})
 
 app.listen(port, () => {
   console.log(
